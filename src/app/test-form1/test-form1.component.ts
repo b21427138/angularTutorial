ae32b723f58c946676a8d7e6503040cda9aacf7d
my name <me@example.com>
2024-09-16 10:15:26 +0300

    testForm1 initialized

--- src/app/test-form1/test-form1.component.ts
+++ src/app/test-form1/test-form1.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test-form1',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './test-form1.component.html',
   styleUrl: './test-form1.component.css'
 })
 export class TestForm1Component {
-
+  name = new FormControl('');
 }
